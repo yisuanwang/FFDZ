e0551cae7db8f5df6c839ff2d950860e335434ed
yisuanwang <77476098+yisuanwang@users.noreply.github.com>
2021-01-26 13:48:36 +0800

    Add files via upload

---
+++ ffdz/index.php
@@ -0,0 +1,33 @@
+﻿<!DOCTYPE html>
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<title>负负得正</title>
+	<meta name="description" content="既然生活这么负能量，何不试试负负得正呢？">
+	<meta name="keywords" content="负能量，网抑云">
+	<meta http-equiv="Cache-Control" content="no-siteapp">
+	<meta name="author" content="yisuanwang@126.com" />
+	<meta property="og:title" content="负负得正" />
+	<meta property="og:url" content="http://limonstudio.top/ffdz" />
+	<meta property="og:image" content="http://limonstudio.top/icon.png" />
+	<meta property="og:site_name" content="负负得正" />
+	<meta property="og:description" content="既然生活这么负能量，何不试试负负得正呢？"/>
+	<link rel="icon" href="/favicon.ico" type="image/x-icon" id="page_favionc">
+	<link href="./img/min.css" rel="stylesheet">
+	<link rel="alternate icon" type="image/png" href="icon.png">
+
+</head>
+<body>
+
+<div class="main-wrapper" style="position: relative; top: -6vh;">
+<div class="container main-sentence justify-content-center text-center">
+		<span id="sentence" style="font-size: 2rem;"><?php require("sct.php"); ?> </span>
+</div>
+</div>
+
+<div style="display:none;">
+<script type="text/javascript" src="https://s9.cnzz.com/z_stat.php?id=5406879&web_id=5406879"></script>
+</div>
+
+</body>
+</html>
